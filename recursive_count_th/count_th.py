b9ddff25dbe66465a06079e42a98928e71e6c69f
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-06-19 17:33:14 -0400

    passing count_th tests

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -16,15 +16,27 @@ def count_th(word, count=0):
 
     # return count
 
-
-    # track how many th you encounter
-    th = "th"
-    if th in word:
-        count += 1
-        word = word.replace('th', '--', 1)
-        count_th(word, count)
+    # # track how many th you encounter
+    # th = "th"
+    # if th in word:
+    #     count += 1
+    #     print(word)
+    #     word = word.replace('th', '--', 1)
+    #     print(word)
+    #     count_th(word, count)
+    # else:
+    #     """ can't figure out why count is printing the correct answer, but returning "None" """
+    #     print("X: ", count)
+    #     return count
+    
+    if len(word) > 1:
+        if word[0:2] == "th":
+            word = word.replace('th', '', 1)
+            print(word)
+            return count_th(word, count + 1)
+        else:
+            word = word[1:]
+            print(word)
+            return count_th(word, count)
     else:
-        """ can't figure out why count is printing the correct answer, but returning "None" """
-        print("X: ", count)
         return count
-
